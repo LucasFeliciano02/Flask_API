ea5b52e54adc5e6e879f2245a3d12aaf2f8f79b2
unknown <LucasFeliciano02@outlook.com>
2022-07-06 09:38:37 -0300

    database

---
+++ Flask_bd/create_database.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+
+conn = sqlite3.connect('enterprise.db')
+
+cursor = conn.cursor()
+
+cursor.execute("""
+               CREATE TABLE empregados (
+                   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                   nome TEXT NOT NULL,
+                   cargo TEXT,
+                   salario REAL
+               );
+               """)
+
+print('TABELA CRIADA!')
+
+
+conn.close()
